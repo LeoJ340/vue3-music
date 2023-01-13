2978b50ce6e1fa1eb1ff972db1d27c31ab990694
LeoJ340 <1015668003@qq.com>
2023-01-13 22:13:27 +0800

    update Aside

--- src/layout/components/Aside.vue
+++ src/layout/components/Aside.vue
@@ -2,14 +2,14 @@
   <el-aside width="200px">
     <el-scrollbar>
       <ul class="menu">
-        <li class="menu-group">
-          <div class="menu-group_title">我的音乐</div>
-          <div class="menu-group_item">
-            <router-link to="/my/playlist/2428211358">我喜欢的音乐</router-link>
-          </div>
-          <div class="menu-group_item">
-            <router-link to="/my/cloud">我的音乐云盘</router-link>
-          </div>
+        <li v-for="menuGroup in menuGroups" class="menu-group">
+          <div class="menu-group_title">{{menuGroup.title}}</div>
+          <router-link v-for="menuItem in menuGroup.list"
+                       class="menu-group_item" :class="{ active: isActive(menuItem.path) }"
+                       :to="menuItem.path">
+            <Component :is="menuItem.icon" theme="outline" size="20" fill="#000000" :strokeWidth="2" />
+            <span style="margin-left: 5px;">{{menuItem.text}}</span>
+          </router-link>
         </li>
       </ul>
     </el-scrollbar>
@@ -18,8 +18,30 @@
 
 <script setup lang="ts">
 import { useRouter } from "vue-router";
-const routes = useRouter().options.routes.filter(route => !route.hidden)
+import { Like, Download } from '@icon-park/vue-next';
 
+const router = useRouter()
+const menuGroups = [
+  {
+    title: '我的音乐',
+    list: [
+      {
+        path: '/my/playlist/2428211358',
+        text: '我喜欢的音乐',
+        icon: Like
+      },
+      {
+        path: '/my/cloud',
+        text: '我的音乐云盘',
+        icon: Download
+      }
+    ]
+  }
+]
+
+function isActive(path: string) {
+  return router.currentRoute.value.fullPath === path
+}
 </script>
 
 <style lang="scss" scoped>
@@ -27,18 +49,21 @@ const routes = useRouter().options.routes.filter(route => !route.hidden)
   background-color: rgb(255, 255, 255);
   border-right: 1px solid rgb(224, 224, 244);
   .menu {
-    padding-left: 20px;
-    padding-right: 5px;
+    padding: 0;
     .menu-group_title {
-      color: rgb(229, 229, 229);
+      padding-left: 20px;
       font-size: 14px;
+      color: rgb(169, 169, 169);
     }
-    .menu-group_item a {
-      display: block;
-      padding: 10px 0;
-    }
-    .menu-group_item a:hover {
-      background-color: rgb(246, 246, 247);
+    .menu-group_item {
+      display: flex;
+      align-items: center;
+      padding: 10px 20px 10px 5px;
+      color: rgb(135, 135, 135);
+      &.active, &:hover {
+        background-color: rgb(246, 246, 247);
+        color: #000000;
+      }
     }
   }
 }
