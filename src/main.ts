8f92a6c4ba99564abcf2e321459f4e5c0313e338
LeoJ340 <1015668003@qq.com>
2023-03-04 17:09:57 +0800

    优化组件引入方式

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-
 import '@/styles/index.css' // global css
 import '@/styles/utils.scss'
 import '@/styles/theme.scss'
@@ -15,5 +12,4 @@ import '@/styles/player.scss'
 const app = createApp(App)
 app.use(router)
 app.use(createPinia())
-app.use(ElementPlus)
 app.mount('#app')
