05b4ef2c54dccf5edc686e9581fbaa273b699222
LeoJ340 <1015668003@qq.com>
2023-03-17 21:53:21 +0800

    build

---
+++ docs/assets/count.b035295c.js
@@ -0,0 +1,1 @@
+function e(r){return r>1e8?`${Math.floor(r/1e8)}\u4EBF`:r>1e7?`${Math.floor(r/1e7)}\u5343\u4E07`:r>1e4?`${Math.floor(r/1e4)}\u4E07`:r}export{e as u};
---
+++ docs/assets/typeFormate.f6de6737.js
@@ -0,0 +1,1 @@
+function i(a){return{id:a.id,name:a.name,al:a.album,alia:a.alias,ar:a.artists,dt:a.duration,noCopyrightRcmd:{type:0,typeDesc:""},publishTime:a.album.publishTime}}export{i as u};
