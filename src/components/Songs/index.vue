cfbc696a09594383861416f22e2cf829ea1bc629
LeoJ340 <1015668003@qq.com>
2023-04-27 00:19:35 +0800

    完成喜欢歌曲功能

--- src/components/Songs/index.vue
+++ src/components/Songs/index.vue
@@ -8,11 +8,10 @@
         <span>{{scope.$index + 1}}</span>
       </template>
     </el-table-column>
-    <!-- TODO：实现功能 -->
     <el-table-column label="操作" width="80">
       <template #default="scope">
         <div style="display: flex; justify-content: space-around;">
-          <el-link :underline="false" title="喜欢">
+          <el-link :underline="false" :title="`${myLikedSongIds.includes(scope.row.id) ? '不' : ''}喜欢`" @click="toLike(scope.row.id, !myLikedSongIds.includes(scope.row.id))">
             <Like v-if="myLikedSongIds.includes(scope.row.id)" theme="filled" size="20" fill="#ec4141" :strokeWidth="2"/>
             <Like v-else theme="outline" size="20" :strokeWidth="2"/>
           </el-link>
@@ -60,19 +59,48 @@ import { useUserStore } from "@/stores/user";
 import {storeToRefs} from "pinia";
 import { useFormatSeconds } from "@/utils/time";
 import {toCommonAlbum} from "@/router/usePush";
+import {likeSong} from "@/api/song";
 
 const props = defineProps<{ songs: Song[] }>()
 
-const { myPlayList } = storeToRefs(useUserStore())
-const myLikedSongIds = myPlayList.value.liked.tracks.map((item: Song) => item.id)
-
-const { push } = usePlayerStore()
+const userStore = useUserStore()
+const { getMyLikedSongIds } = userStore
+const { myLikedSongIds } = storeToRefs(userStore)
 
 // 双击播放
+const { push } = usePlayerStore()
 function dblclickPlay(song: Song) {
   if (song.noCopyrightRcmd) return
   const starIndex = props.songs.findIndex(item => item.id === song.id)
   push(props.songs.filter(item => !item.noCopyrightRcmd), { replace: true, starIndex, trigger: 'doubleClick' })
+}
+
+// 喜欢&不喜欢歌曲
+function toLike(id: number, like: boolean) {
+  if (!like) {
+    ElMessageBox.alert('确定将选中歌曲从我的喜欢中删除？', '', {
+      confirmButtonText: '确定',
+      center: true,
+      callback: () => {
+        ElMessage({
+          message: '接口功能异常，操作失败',
+          type: 'error',
+          duration: 1000,
+          center: true
+        })
+      },
+    })
+    return
+  }
+  likeSong(id, like).then(() => {
+    ElMessage({
+      message: '已添加到我的喜欢',
+      type: 'success',
+      duration: 1000,
+      center: true
+    })
+    getMyLikedSongIds()
+  })
 }
 </script>
 
